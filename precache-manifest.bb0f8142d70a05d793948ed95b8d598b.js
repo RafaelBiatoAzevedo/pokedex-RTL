81081e78eaae0a317cbe7a63348bd55c71a13b77
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-11 07:12:48 -0300

    Updates

---
+++ precache-manifest.bb0f8142d70a05d793948ed95b8d598b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "febff5302831b7ad6b3828f484309525",
+    "url": "/pokedex-RTL/index.html"
+  },
+  {
+    "revision": "be720a4808318c8eea8f",
+    "url": "/pokedex-RTL/static/css/main.021039b7.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/pokedex-RTL/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokedex-RTL/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be720a4808318c8eea8f",
+    "url": "/pokedex-RTL/static/js/main.90d5f5c4.chunk.js"
+  },
+  {
+    "revision": "c2d7898cc1f4e067d3be",
+    "url": "/pokedex-RTL/static/js/runtime-main.a2f5936d.js"
+  },
+  {
+    "revision": "8df9e9f3d15343180a7576506a2aff65",
+    "url": "/pokedex-RTL/static/media/capa.8df9e9f3.jpg"
+  }
+]);
